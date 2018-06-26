ba40a195c06a3b843c0dd91b75beab4d765176e0
lindongwu <lindongwu@memect.co>
2018-06-26 18:05:08 +0800

    refactor: move resolver to object method

--- server/resolvers/Query/block.js
+++ server/resolvers/Query/block.js
@@ -14,38 +14,36 @@ export function getBlockByBlockNum(blockNum: number) {
   return get(`/blocks?block_num=${blockNum}`);
 }
 
-async function block(
-  _: any,
-  { blockNum, id, blockNumOrID }: { blockNum?: number, id?: string, blockNumOrID: number | string },
-) {
-  if (typeof blockNum === 'number') return getBlockByBlockNum(blockNum);
-  if (typeof blockNumOrID === 'number' || Number.isFinite(Number(blockNumOrID)))
-    return get(`/blocks?block_num=${blockNumOrID}`);
-  // 尝试把 blockID 转换成「区块高度 blockNum」
-  const blockIDSearchResults = await searchKeyWord({ keyWord: id || blockNumOrID, type: 'block' });
-
-  const firstBlockInResult = getFirstBlockIdFromBlockListResponse(blockIDSearchResults);
-  if (firstBlockInResult) {
-    return get(`/blocks?block_num=${firstBlockInResult.blockNum}`);
-  }
-
-  throw new Error(
-    `${blockNumOrID} is not a block Number nor a block ID.\nAnd blockNum === ${String(blockNum)}\nid === ${String(id)}`,
-  );
-}
-
-async function blocks(_: any, { page, pageSize }: { page?: number, pageSize?: number }) {
-  const gotoPage = page ? page - 1 : 0;
-  const data: ListResponse<Block> = await get(`/blocks?page=${gotoPage}&size=${pageSize || 60}`);
-  const {
-    content,
-    page: { totalElements },
-  } = data;
-  return content;
-  // this.setPage({ current: gotoPage || 0, total: totalElements });
-}
-
 export default {
-  block,
-  blocks,
+  async block(
+    _: any,
+    { blockNum, id, blockNumOrID }: { blockNum?: number, id?: string, blockNumOrID: number | string },
+  ) {
+    if (typeof blockNum === 'number') return getBlockByBlockNum(blockNum);
+    if (typeof blockNumOrID === 'number' || Number.isFinite(Number(blockNumOrID)))
+      return get(`/blocks?block_num=${blockNumOrID}`);
+    // 尝试把 blockID 转换成「区块高度 blockNum」
+    const blockIDSearchResults = await searchKeyWord({ keyWord: id || blockNumOrID, type: 'block' });
+
+    const firstBlockInResult = getFirstBlockIdFromBlockListResponse(blockIDSearchResults);
+    if (firstBlockInResult) {
+      return get(`/blocks?block_num=${firstBlockInResult.blockNum}`);
+    }
+
+    throw new Error(
+      `${blockNumOrID} is not a block Number nor a block ID.\nAnd blockNum === ${String(blockNum)}\nid === ${String(
+        id,
+      )}`,
+    );
+  },
+  async blocks(_: any, { page, pageSize }: { page?: number, pageSize?: number }) {
+    const gotoPage = page ? page - 1 : 0;
+    const data: ListResponse<Block> = await get(`/blocks?page=${gotoPage}&size=${pageSize || 60}`);
+    const {
+      content,
+      page: { totalElements },
+    } = data;
+    return content;
+    // this.setPage({ current: gotoPage || 0, total: totalElements });
+  },
 };
--- server/resolvers/Query/search.js
+++ server/resolvers/Query/search.js
@@ -9,42 +9,40 @@ export async function searchKeyWord(param: { keyWord: string, type: string }) {
   return get(`/search?type=${type}&id=${keyWord}`);
 }
 
-export async function search(_: any, { keyWord }: { keyWord: string }) {
-  const { getBlockByBlockNum, getFirstBlockIdFromBlockListResponse } = await import('./block');
-
-  if (Number.isFinite(Number(keyWord))) {
-    return { __typename: 'Block', ...(await getBlockByBlockNum(Number(keyWord))) };
-  }
-
-  // 搜索黏贴的时候可能带上了空格
-  keyWord = keyWord.replace(/\s/g, '');
-  if (keyWord.replace(/\s/g, '').length === 64) {
-    // 长度为 64 的可能是 blockId，或者 transactionId
-    // 尝试把 blockID 转换成「区块高度 blockNum」
-    const blockIDSearchResults = await searchKeyWord({ keyWord, type: 'block' });
-    const firstBlockInResult = getFirstBlockIdFromBlockListResponse(blockIDSearchResults);
-    if (
-      firstBlockInResult &&
-      firstBlockInResult.blockId === keyWord &&
-      typeof firstBlockInResult.blockNum === 'number'
-    ) {
-      return { __typename: 'Block', ...(await getBlockByBlockNum(firstBlockInResult.blockNum)) };
-    }
-    const { getTransactionByID } = await import('./transaction');
-    return { __typename: 'Transaction', ...(await getTransactionByID(keyWord)) };
-  }
-  if (keyWord.length === 53) {
-    // 还可能是账号公钥
-    const data = await postEOS('/history/get_key_accounts', { key: keyWord });
-    if (data.accountNames.length > 0) {
-      const { getAccountByName } = await import('./account');
-      // 目前先取这个公钥下的第一个账号，如果真的会有多个账号我们可以返回列表？
-      return { __typename: 'Account', ...getAccountByName(data.accountNames[0]) };
-    }
-  }
-  // 其他目前默认是账户名
-}
-
 export default {
-  search,
+  async search(_: any, { keyWord }: { keyWord: string }) {
+    const { getBlockByBlockNum, getFirstBlockIdFromBlockListResponse } = await import('./block');
+
+    if (Number.isFinite(Number(keyWord))) {
+      return { __typename: 'Block', ...(await getBlockByBlockNum(Number(keyWord))) };
+    }
+
+    // 搜索黏贴的时候可能带上了空格
+    keyWord = keyWord.replace(/\s/g, '');
+    if (keyWord.replace(/\s/g, '').length === 64) {
+      // 长度为 64 的可能是 blockId，或者 transactionId
+      // 尝试把 blockID 转换成「区块高度 blockNum」
+      const blockIDSearchResults = await searchKeyWord({ keyWord, type: 'block' });
+      const firstBlockInResult = getFirstBlockIdFromBlockListResponse(blockIDSearchResults);
+      if (
+        firstBlockInResult &&
+        firstBlockInResult.blockId === keyWord &&
+        typeof firstBlockInResult.blockNum === 'number'
+      ) {
+        return { __typename: 'Block', ...(await getBlockByBlockNum(firstBlockInResult.blockNum)) };
+      }
+      const { getTransactionByID } = await import('./transaction');
+      return { __typename: 'Transaction', ...(await getTransactionByID(keyWord)) };
+    }
+    if (keyWord.length === 53) {
+      // 还可能是账号公钥
+      const data = await postEOS('/history/get_key_accounts', { key: keyWord });
+      if (data.accountNames.length > 0) {
+        const { getAccountByName } = await import('./account');
+        // 目前先取这个公钥下的第一个账号，如果真的会有多个账号我们可以返回列表？
+        return { __typename: 'Account', ...getAccountByName(data.accountNames[0]) };
+      }
+    }
+    // 其他目前默认是账户名
+  },
 };
