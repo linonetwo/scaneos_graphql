17c68de436df93bedaaa950d19b451870b18b7d7
lindongwu <lindongwu@memect.co>
2018-06-28 02:51:50 +0800

    feat: naively merge twu REST API

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -1,6 +1,5 @@
 import Query from './Query';
 import { SearchResult } from './Query/search';
-import { BlockChainStatus, EOSGlobalStats } from './Query/aggregation';
 import { Block } from './Query/block';
 import { Transaction } from './Query/transaction';
 import { Account } from './Query/account';
@@ -9,8 +8,6 @@ import { Account } from './Query/account';
 export default {
   Query,
   SearchResult,
-  BlockChainStatus,
-  EOSGlobalStats,
   Account,
   Block,
   Transaction,
