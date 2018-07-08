7bc12fbc4d98421e41ef5a684c1a2b7e892005c9
linonetwo <linonetwo012@gmail.com>
2018-07-08 23:20:59 +0800

    feat: load wiki pages

--- server/resolvers/Query/article.js
+++ server/resolvers/Query/article.js
@@ -2,6 +2,15 @@
 import { size as objSize } from 'lodash';
 import { getCMS, PAGE_SIZE_DEFAULT } from '../../../API.config';
 
+export function formatWiki(wiki) {
+  return {
+    field: wiki.field,
+    title: wiki.en,
+    titleZh: wiki.zh,
+    content: wiki.enMeaning,
+    contentZh: wiki.zhMeaning,
+  };
+}
 export const Article = {
   async producerInfo({ bp: bpIDInDB }: { bp?: number }) {
     const { data } = await getCMS(`tables/bp/rows/${bpIDInDB}`);
@@ -28,6 +37,34 @@ export default {
     return {
       articles,
       pageInfo: { totalPages: Math.ceil(Published / limit) },
+    };
+  },
+  async wiki(_: any, { field }: { field: string }) {
+    const {
+      data: [wiki],
+    } = await getCMS(`tables/translation/rows?filters[field][eq]=${field}`);
+    return formatWiki(wiki);
+  },
+  async wikis(_: any, { keyWord, page, size }: { keyWord?: string, page?: number, size?: number }) {
+    let offset = 0;
+    let limit = PAGE_SIZE_DEFAULT;
+    if (typeof size === 'number') {
+      limit = size;
+    }
+    if (typeof page === 'number') {
+      offset = page * limit;
+    }
+    const searchQuery = keyWord
+      ? `&filters[content][contains]=${keyWord}&filters[title][logical]=or&filters[title][contains]=${keyWord}`
+      : '';
+    const {
+      data,
+      meta: { Published },
+    } = await getCMS(`tables/translation/rows?columns=field,zh,en&offset=${offset}&limit=${limit}${searchQuery}`);
+
+    return {
+      wikis: data.map(formatWiki),
+      pageInfo: { totalPages: Math.ceil(Published / limit), totalElements: Published, page, size },
     };
   },
 };
