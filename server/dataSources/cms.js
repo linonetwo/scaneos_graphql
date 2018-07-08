efe0d60f3fa306d592c078b96fd3e291e8a2fa53
linonetwo <linonetwo012@gmail.com>
2018-07-08 12:49:10 +0800

    feat: manully add a cache

--- server/dataSources/cms.js
+++ server/dataSources/cms.js
@@ -1,4 +1,6 @@
 import { RESTDataSource } from 'apollo-datasource-rest';
+import cache from 'memory-cache';
+
 import { CMS_API } from '../../API.config';
 
 export default class CMS extends RESTDataSource {
@@ -13,7 +15,12 @@ export default class CMS extends RESTDataSource {
   }
 
   async getBPDescriptionList() {
-    const { data } = await this.get('tables/bp/rows');
-    return data;
+    const route = 'tables/bp/rows';
+    let response = cache.get(route)
+    if (!response) {
+      response = await this.get(route);
+      cache.put(route, response, 1000 * 3600 * 3);
+    }
+    return response.data;
   }
 }
