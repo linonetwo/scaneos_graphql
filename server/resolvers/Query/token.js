f7b13f3ddcd95512e0d3c7ab72cc61fbfb99a8e0
linonetwo <linonetwo012@gmail.com>
2018-07-19 23:55:02 +0800

    bugfix: get all tokens

--- server/resolvers/Query/token.js
+++ server/resolvers/Query/token.js
@@ -3,7 +3,7 @@ import get, { PAGE_SIZE_DEFAULT } from '../../../API.config';
 
 export default {
   tokens(_: any, { page, size }: { page?: number, size?: number }) {
-    return get(`/actions?requiredata=true&type=create&page=${page || 0}&size=${size || PAGE_SIZE_DEFAULT}`).then(
+    return get(`/actions?requiredata=true&type=create&page=${page || 0}&size=${size || 9999}`).then(
       ({ content, page: { number, size: pageSize, totalPages, totalElements } }) => ({
         tokens: content.map(({ createdAt, data }) => ({ createdAt, ...data })),
         pageInfo: { totalPages, totalElements, page: number, size: pageSize },
