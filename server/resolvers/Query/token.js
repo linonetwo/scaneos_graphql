becf225a3e8afb272567290a1ca08040639ec46b
linonetwo <linonetwo012@gmail.com>
2018-07-20 14:10:08 +0800

    chore: token using timestamp

--- server/resolvers/Query/token.js
+++ server/resolvers/Query/token.js
@@ -3,9 +3,9 @@ import get, { PAGE_SIZE_DEFAULT } from '../../../API.config';
 
 export default {
   tokens(_: any, { page, size }: { page?: number, size?: number }) {
-    return get(`/actions?requiredata=true&type=create&page=${page || 0}&size=${size || 9999}`).then(
+    return get(`/actions?requiredata=true&type=create&page=${page || 0}&size=${size || PAGE_SIZE_DEFAULT}`).then(
       ({ content, page: { number, size: pageSize, totalPages, totalElements } }) => ({
-        tokens: content.map(({ createdAt, data }) => ({ createdAt, ...data })),
+        tokens: content.map(({ timestamp, data }) => ({ timestamp, ...data })),
         pageInfo: { totalPages, totalElements, page: number, size: pageSize },
       }),
     );
