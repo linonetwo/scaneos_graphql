d6ec1101e4ac46ad5d396c2d6e6e4d064d3bbf70
linonetwo <linonetwo012@gmail.com>
2018-08-06 16:25:31 +0800

    feat: make actions list searchable

--- server/resolvers/Query/action.js
+++ server/resolvers/Query/action.js
@@ -2,6 +2,7 @@
 import { size as objSize } from 'lodash';
 import utf8 from 'utf8';
 import qs from 'query-string';
+import { compareDesc } from 'date-fns';
 import get, { PAGE_SIZE_DEFAULT } from '../../../API.config';
 
 export const formatActionData = ({ actionId, transactionId, data, ...rest }) => ({
@@ -21,7 +22,29 @@ export const Action = {
   },
 };
 export default {
-  actions(_: any, { page = 0, size = PAGE_SIZE_DEFAULT }: { page?: number, size?: number }) {
+  actions(_: any, { filterBy, page = 0, size = PAGE_SIZE_DEFAULT }: { filterBy?: { match: string }, page?: number, size?: number }) {
+    // search specific text
+    if (filterBy.match) {
+      const query = qs.stringify({
+        page,
+        size,
+        match: filterBy.match,
+        matchasphrase: false,
+      });
+      return get(`/actions/text?${query}`).then(
+        ({ content, page: { number, size: pageSize, totalPages, totalElements } }) => ({
+          actions: content.map(formatActionData).sort((a, b) => compareDesc(a.createdAt, b.createdAt)),
+          pageInfo: {
+            totalPages,
+            totalElements,
+            page: number,
+            size: pageSize,
+            filterBy,
+          },
+        }),
+      );
+    }
+    // return all kinds of actions
     const query = qs.stringify({
       page,
       size,
